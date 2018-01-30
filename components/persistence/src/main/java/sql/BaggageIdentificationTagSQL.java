219984b8c6078539f5e740c0dcee2a20dd38c879
Annika <annika.schatz21@web.de>
2018-01-30 22:11:59 +0100

    changed BaggageIDSQL and ContainerSQL

--- components/persistence/src/main/java/sql/BaggageIdentificationTagSQL.java
+++ components/persistence/src/main/java/sql/BaggageIdentificationTagSQL.java
@@ -1,4 +1,80 @@
 package sql;
 
+import base.BaggageIdentificationTag;
+import engine.LogEngine;
+import main.Database;
+
 public class BaggageIdentificationTagSQL {
+    Database instance = new Database();
+
+    public void dropTableBaggageIdentificationTag(LogEngine logEngine) {
+        String sqlStatement = "DROP TABLE BaggageIdentificationTag IF EXISTS";
+        logEngine.write("main.Database", "dropTableBaggageIdentificationTag", "-", sqlStatement);
+        instance.update(sqlStatement);
+    }
+
+    public void createTableBaggageIdentificationTag(LogEngine logEngine) {
+        StringBuilder sqlStringBuilder = new StringBuilder();
+        sqlStringBuilder.append("CREATE TABLE baggageIdentificationTag").append(" ( ");
+        sqlStringBuilder.append("id TEXT NOT NULL").append(",");
+        sqlStringBuilder.append("boardingPass TEXT NOT NULL").append(",");
+        sqlStringBuilder.append("carrier TEXT NOT NULL").append(",");
+        sqlStringBuilder.append("sequence INT NOT NULL").append(",");
+        sqlStringBuilder.append("via01 TEXT NOT NULL").append(",");
+        sqlStringBuilder.append("via02 TEXT NOT NULL").append(",");
+        sqlStringBuilder.append("via03 TEXT NOT NULL").append(",");
+        sqlStringBuilder.append("baggage TEXT NOT NULL").append(",");
+        sqlStringBuilder.append("isLocal TEXT NOT NULL").append(",");
+        sqlStringBuilder.append("isTransfer TEXT NOT NULL").append(",");
+        sqlStringBuilder.append("isTransferFirst TEXT NOT NULL").append(",");
+        sqlStringBuilder.append("barCode TEXT NOT NULL");
+        sqlStringBuilder.append("PRIMARY KEY (id)");
+        sqlStringBuilder.append(" )");
+        logEngine.write("main.Database", "createTableBaggageIdentificationTag", "-", sqlStringBuilder.toString());
+        instance.update(sqlStringBuilder.toString());
+    }
+
+
+    public String buildInsertSQLStatement(BaggageIdentificationTag baggageIdentificationTag) {
+        StringBuilder sqlStringBuilder = new StringBuilder();
+        sqlStringBuilder.append("INSERT INTO baggageIdentificationTag (id,boardingPass,carrier,sequence,via01,via02,via03,baggage,specialGood,isLocal,isTransfer,isTransferFirst,barCode) VALUES (");
+        sqlStringBuilder.append("'").append(baggageIdentificationTag.getBoardingPass().getId()).append("'").append(",");
+        sqlStringBuilder.append("'").append(baggageIdentificationTag.getCarrier().toString()).append("'").append(",");
+        sqlStringBuilder.append(baggageIdentificationTag.getSequence()).append(",");
+        sqlStringBuilder.append("'").append(baggageIdentificationTag.getVia01()).append("'").append(",");
+        sqlStringBuilder.append("'").append(baggageIdentificationTag.getVia02()).append("'").append(",");
+        sqlStringBuilder.append("'").append(baggageIdentificationTag.getVia03()).append("'").append(",");
+        sqlStringBuilder.append("'").append(baggageIdentificationTag.getBaggage().getUUID()).append("'").append(",");
+        sqlStringBuilder.append("'").append(baggageIdentificationTag.getSpecialGood().getSpecialGoodType()).append("'").append(",");
+        sqlStringBuilder.append("'").append(baggageIdentificationTag.isLocal()).append("'").append(",");
+        sqlStringBuilder.append("'").append(baggageIdentificationTag.isTransfer()).append("'").append(",");
+        sqlStringBuilder.append("'").append(baggageIdentificationTag.isTransferFirst()).append("'").append(",");
+        sqlStringBuilder.append("'").append(baggageIdentificationTag.getBarCode()).append("'");
+        sqlStringBuilder.append(")");
+        return sqlStringBuilder.toString();
+    }
+
+    public void insert(BaggageIdentificationTag baggageIdentificationTag, LogEngine logEngine) {
+        logEngine.write("main.Database", "insert", "BaggageIdentificationTag = " + baggageIdentificationTag.getId(), buildInsertSQLStatement(baggageIdentificationTag));
+        instance.update(buildInsertSQLStatement(baggageIdentificationTag));
+    }
+
+    public String buildUpdateSQLStatement(BaggageIdentificationTag baggageIdentificationTag) {
+        StringBuilder sqlStringBuilder = new StringBuilder();
+        sqlStringBuilder.append("UPDATE baggageIdentificationTag SET boardingPass = '").append(baggageIdentificationTag.getBoardingPass()).append("'").append(",");
+        sqlStringBuilder.append("carrier = ").append(baggageIdentificationTag.getCarrier().toString()).append(",");
+        sqlStringBuilder.append("sequence = ").append(baggageIdentificationTag.getSequence()).append(",");
+        sqlStringBuilder.append("via01 = ").append(baggageIdentificationTag.getVia01()).append(",");
+        sqlStringBuilder.append("via02 = ").append(baggageIdentificationTag.getVia02()).append(",");
+        sqlStringBuilder.append("via03 = ").append(baggageIdentificationTag.getVia03()).append(",");
+        sqlStringBuilder.append("baggage = ").append(baggageIdentificationTag.getBaggage().getUUID()).append(",");
+        sqlStringBuilder.append("specialGood = ").append(baggageIdentificationTag.getSpecialGood().getSpecialGoodType()).append(",");
+        sqlStringBuilder.append("isLocal = ").append(baggageIdentificationTag.isLocal()).append(",");
+        sqlStringBuilder.append("isTransfer = ").append(baggageIdentificationTag.isTransfer()).append(",");
+        sqlStringBuilder.append("isTransferFirst = ").append(baggageIdentificationTag.isTransferFirst()).append(",");
+        sqlStringBuilder.append("barCode = ").append(baggageIdentificationTag.getBarCode()).append(",");
+        sqlStringBuilder.append("WHERE id = '").append(baggageIdentificationTag.getId()).append("'");
+        return sqlStringBuilder.toString();
+    }
+}
 }
--- components/persistence/src/main/java/sql/ContainerSQL.java
+++ components/persistence/src/main/java/sql/ContainerSQL.java
@@ -1,7 +1,6 @@
 package sql;
 
 
-import base.Baggage;
 import base.Container;
 import engine.LogEngine;
 import main.Database;
@@ -10,46 +9,57 @@ public class ContainerSQL {
 
     Database instance = new Database();
 
-    public void dropTableBaggage(LogEngine logEngine) {
-        String sqlStatement = "DROP TABLE baggage IF EXISTS";
-        logEngine.write("main.Database", "dropTableBaggage", "-", sqlStatement);
+    public void dropTableContainer(LogEngine logEngine) {
+        String sqlStatement = "DROP TABLE container IF EXISTS";
+        logEngine.write("main.Database", "dropTableContainer", "-", sqlStatement);
         instance.update(sqlStatement);
     }
 
-    public void createTableBaggage(LogEngine logEngine) {
+    public void createTableContainer(LogEngine logEngine) {
         StringBuilder sqlStringBuilder = new StringBuilder();
-        sqlStringBuilder.append("CREATE TABLE baggage").append(" ( ");
-        sqlStringBuilder.append("uuid VARCHAR(36) NOT NULL").append(",");
-        sqlStringBuilder.append("content VARCHAR(50000) NOT NULL").append(",");
-        sqlStringBuilder.append("weight INT NOT NULL").append(",");
-        sqlStringBuilder.append("PRIMARY KEY (uuid)");
+        sqlStringBuilder.append("CREATE TABLE container").append(" ( ");
+        sqlStringBuilder.append("type VARCHAR(50000) NOT NULL").append(",");
+        sqlStringBuilder.append("id VARCHAR(36) NOT NULL").append(",");
+        sqlStringBuilder.append("category VARCHAR(50000) NOT NULL").append(",");
+        sqlStringBuilder.append("profile VARCHAR(50000) NOT NULL").append(",");
+        sqlStringBuilder.append("barCode VARCHAR(50000) NOT NULL").append(",");
+        sqlStringBuilder.append("qrCode VARCHAR(50000) NOT NULL").append(",");
+        sqlStringBuilder.append("maxLugage INT NOT NULL").append(",");
+        sqlStringBuilder.append("PRIMARY KEY (id)");
         sqlStringBuilder.append(" )");
-        logEngine.write("main.Database", "createTableBaggage", "-", sqlStringBuilder.toString());
+        logEngine.write("main.Database", "createTableContainer", "-", sqlStringBuilder.toString());
         instance.update(sqlStringBuilder.toString());
     }
 
 
     public String buildInsertSQLStatement(Container container) {
         StringBuilder sqlStringBuilder = new StringBuilder();
-        sqlStringBuilder.append("INSERT INTO baggage (uuid,content,weight) VALUES (");
-        sqlStringBuilder.append("'").append(baggage.getUUID()).append("'").append(",");
-        sqlStringBuilder.append("'").append(baggage.getContent()).append("'").append(",");
-        sqlStringBuilder.append(baggage.getWeight()).append(",");
-        sqlStringBuilder.append("'").append(baggage.getBaggageType()).append("'");
+        sqlStringBuilder.append("INSERT INTO container (type,id,category,profile,barCode,qrCode,maxLugage) VALUES (");
+        sqlStringBuilder.append("'").append(container.getType().toString()).append("'").append(",");
+        sqlStringBuilder.append("'").append(container.getId()).append("'").append(",");
+        sqlStringBuilder.append("'").append(container.getCategory().toString()).append("'").append(",");
+        sqlStringBuilder.append("'").append(container.getProfile().toString()).append("'").append(",");
+        sqlStringBuilder.append("'").append(container.getBarCodeIDCategory()).append("'").append(",");
+        sqlStringBuilder.append("'").append(container.getQrCodeIDECategory()).append("'").append(",");
+        sqlStringBuilder.append(container.getMaximumNumberOfBaggages());
         sqlStringBuilder.append(")");
         return sqlStringBuilder.toString();
     }
 
     public void insert(Container container, LogEngine logEngine) {
-        logEngine.write("main.Database", "insert", "baggage = " + baggage.getUUID(), buildInsertSQLStatement(baggage));
+        logEngine.write("main.Database", "insert", "container = " + container.getId(), buildInsertSQLStatement(container));
         instance.update(buildInsertSQLStatement(container));
     }
 
     public String buildUpdateSQLStatement(Container container) {
         StringBuilder sqlStringBuilder = new StringBuilder();
-        sqlStringBuilder.append("UPDATE baggage SET content = '").append(baggage.getContent()).append("'").append(",");
-        sqlStringBuilder.append("weight = ").append(baggage.getWeight()).append(",");
-        sqlStringBuilder.append("WHERE uuid = '").append(baggage.getUUID()).append("'");
+        sqlStringBuilder.append("UPDATE container SET type = '").append(container.getType().toString()).append("'").append(",");
+        sqlStringBuilder.append("category = ").append(container.getCategory().toString()).append(",");
+        sqlStringBuilder.append("profile = ").append(container.getProfile().toString()).append(",");
+        sqlStringBuilder.append("barCode = ").append(container.getBarCodeIDCategory()).append(",");
+        sqlStringBuilder.append("category = ").append(container.getQrCodeIDECategory()).append(",");
+        sqlStringBuilder.append("maxLugage = ").append(container.getMaximumNumberOfBaggages()).append(",");
+        sqlStringBuilder.append("WHERE id = '").append(container.getId()).append("'");
         return sqlStringBuilder.toString();
     }
 }
