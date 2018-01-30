2a5c589bed29de584583b9d4eb4242af8f9706df
Marc <marc@h8m.eu>
2018-01-30 13:27:35 +0100

    Port implemented

--- components/customs/src/main/java/Customs.java
+++ components/customs/src/main/java/Customs.java
@@ -1,27 +1,54 @@
 import java.util.ArrayList;
 import DummyFiles.*;
 
-public class Customs implements ICustoms {
+public class Customs{
+
+    private static Customs instance = new Customs();
+
+    public Port port;
+
+    private Customs(){
+        port = new Port();
+        numberOfPassportsVerified=0;
+        numberOfBaggageScanned=0;
+        boardingPassList=new ArrayList<>();
+        invoiceList=new ArrayList<>();
+    }
+
+    public static Customs getInstance(){ return instance; }
 
     private int numberOfPassportsVerified;
     private int numberOfBaggageScanned;
     private ArrayList<BoardingPass> boardingPassList;
     private ArrayList<Invoice> invoiceList;
-    @Override
-    public boolean verify(Passport passport, BoardingPass boardingPass, Invoice parameter)
+
+    public class Port implements ICustoms{
+
+        @Override
+        public boolean verify(Passport passport, BoardingPass boardingPass, Invoice parameter) { return innerMethodVerify(passport, boardingPass, parameter); }
+
+        @Override
+        public boolean scan(Baggage baggage) { return innerMethodScan(baggage); }
+
+
+        @Override
+        public void notifyGroundOperations(CustomsReceipt customsReceipt) { innerMethodNotifyGroundOperations(customsReceipt); }
+        }
+    }
+    public boolean innerMethodVerify(Passport passport, BoardingPass boardingPass, Invoice parameter)
     {
+        if (passport.getPassanger() == boardingPass.getPassanger()) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean innerMethodScan(Baggage baggage) {
 
         return false;
     }
 
-    @Override
-    public boolean scan(Baggage baggage) {
-
-        return false;
-    }
-
-    @Override
-    public void notifyGroundOperations(CustomsReceipt customsReceipt) {
+    public void innerMethodNotifyGroundOperations(CustomsReceipt customsReceipt) {
 
     }
 }
