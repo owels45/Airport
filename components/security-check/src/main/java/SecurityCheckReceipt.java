fc6d4a54ce6b8b5f69f6f50b5850ad82770a3904
Robert Zebec <zebec.robert@gmail.com>
2018-01-30 20:59:03 +0100

    Added some comments to the security check

--- components/security-check/src/main/java/SecurityCheckReceipt.java
+++ components/security-check/src/main/java/SecurityCheckReceipt.java
@@ -5,37 +5,37 @@ import java.util.ArrayList;
 
 public class SecurityCheckReceipt {
     /**
-     * The Employee
+     * The Employee.
      */
     private Employee employee;
     /**
-     * The Cotton Pads
+     * The Cotton Pads.
      */
     private ArrayList<CottonPad> cottonPads;
     /**
-     * The Number of Passengers Scanned
+     * The Number of Passengers Scanned.
      */
     private int numberOfPassengerScanned;
     /**
-     * The Number of Passengers Scanned for Explosives
+     * The Number of Passengers Scanned for Explosives.
      */
     private int numberOfPassengerScannedExplosivesTraceDetector;
     /**
-     * The Number of Bags Scanned
+     * The Number of Bags Scanned.
      */
     private int numberOfBaggageScanned;
     /**
-     * The Number of Dangerous Bags
+     * The Number of Dangerous Bags.
      */
     private int numberOfDangerousBaggage;
     /**
-     * The Constructor of the Security Check Receipt
-     * @param employee The Employee
-     * @param cottonPads The Cotton Pads
-     * @param numberOfPassengerScanned The Number of Passengers Scanned
-     * @param numberOfPassengerScannedExplosivesTraceDetector The Number of Passengers Scanned Explosives
-     * @param numberOfBaggageScanned The Number of Bags Scanned
-     * @param numberOfDangerousBaggage The Number of Dangerous Bags
+     * The Constructor of the Security Check Receipt.
+     * @param employee The Employee.
+     * @param cottonPads The Cotton Pads.
+     * @param numberOfPassengerScanned The Number of Passengers Scanned.
+     * @param numberOfPassengerScannedExplosivesTraceDetector The Number of Passengers Scanned Explosives.
+     * @param numberOfBaggageScanned The Number of Bags Scanned.
+     * @param numberOfDangerousBaggage The Number of Dangerous Bags.
      */
     public SecurityCheckReceipt(Employee employee, ArrayList<CottonPad> cottonPads, int numberOfPassengerScanned, int numberOfPassengerScannedExplosivesTraceDetector, int numberOfBaggageScanned, int numberOfDangerousBaggage) {
         this.employee = employee;
@@ -46,61 +46,69 @@ public class SecurityCheckReceipt {
         this.numberOfDangerousBaggage = numberOfDangerousBaggage;
     }
     /**
-     * Increments the Number of Passengers Scanned By One
+     * Increments the Number of Passengers Scanned By One.
      */
     public void incrementNumberOfPassengerScannedByOne() {
         this.numberOfPassengerScanned++;
     }
     /**
-     * Increments the Number of Passengers Scanned for Explosives By One
+     * Increments the Number of Passengers Scanned for Explosives By One.
      */
     public void incrementNumberOfPassengerScannedExplosivesTraceDetectorByOne() {
         this.numberOfPassengerScannedExplosivesTraceDetector++;
     }
     /**
-     * Increments the Number of Bags Scanned By One
+     * Increments the Number of Bags Scanned By One.
      */
     public void incrementNumberOfBaggageScannedByOne() {
         this.numberOfBaggageScanned++;
     }
     /**
-     * Increments the Number of Dangerous Bags By One
+     * Increments the Number of Dangerous Bags By One.
      */
     public void incrementNumberOfDangerousBaggageByOne() {
         this.numberOfDangerousBaggage++;
     }
     /**
-     * Get the Employee
-     * @return The Employee
+     * Get the Employee.
+     * @return The Employee.
      */
     public Employee getEmployee() {
         return employee;
     }
-
     /**
-     * Get The Cotton Pads
-     * @return The Cotton Pads
+     * Get The Cotton Pads.
+     * @return The Cotton Pads.
      */
     public ArrayList<CottonPad> getCottonPads() {
         return cottonPads;
     }
-
     /**
-     * Get The Number of Passengers Scanned
-     * @return The Number of Passengers Scanned
+     * Get The Number of Passengers Scanned.
+     * @return The Number of Passengers Scanned.
      */
     public int getNumberOfPassengerScanned() {
         return numberOfPassengerScanned;
     }
 
+    /**
+     * Get The Number of Passengers Scanned for Explosives.
+     * @return The Number of Passengers Scanned for Explosives.
+     */
     public int getNumberOfPassengerScannedExplosivesTraceDetector() {
         return numberOfPassengerScannedExplosivesTraceDetector;
     }
-
+    /**
+     * Get The Number of Scanned Bags.
+     * @return The Number of Scanned Bags.
+     */
     public int getNumberOfBaggageScanned() {
         return numberOfBaggageScanned;
     }
-
+    /**
+     * Get The Number of Dangerous Bags.
+     * @return The Number of Dangerous Bags.
+     */
     public int getNumberOfDangerousBaggage() {
         return numberOfDangerousBaggage;
     }
