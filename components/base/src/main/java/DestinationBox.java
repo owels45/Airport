06156953026a912f1fa2d131df666193c7e777a8
MakroCow <yoco.lie@gmail.com>
2018-01-30 12:44:07 +0100

    Added right half of base classes from pdf

---
+++ components/base/src/main/java/DestinationBox.java
@@ -0,0 +1,29 @@
+public class DestinationBox {
+
+    private String barCode;
+    private ArrayList<Baggage> baggegeList;
+
+    public String getBarCode() {
+        return barCode;
+    }
+
+    public void setBarCode(String barCode) {
+        this.barCode = barCode;
+    }
+
+    public ArrayList<Baggage> getBaggegeList() {
+        return baggegeList;
+    }
+
+    public void setBaggegeList(ArrayList<Baggage> baggegeList) {
+        this.baggegeList = baggegeList;
+    }
+
+    @Override
+    public String toString() {
+        return "DestinationBox{" +
+                "barCode='" + barCode + '\'' +
+                ", baggegeList=" + baggegeList +
+                '}';
+    }
+}
---
+++ components/base/src/main/java/PassengerList.java
@@ -0,0 +1,23 @@
+public class PassengerList {
+
+    private ArrayList<Passenger> passengerList;
+
+    public PassengerList(ArrayList<Passenger> passengerList) {
+        this.passengerList = passengerList;
+    }
+
+    public ArrayList<Passenger> getPassengerList() {
+        return passengerList;
+    }
+
+    public void setPassengerList(ArrayList<Passenger> passengerList) {
+        this.passengerList = passengerList;
+    }
+
+    @Override
+    public String toString() {
+        return "PassengerList{" +
+                "passengerList=" + passengerList +
+                '}';
+    }
+}
