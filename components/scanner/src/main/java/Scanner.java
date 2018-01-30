f2db631a9e6df01985501f566a1f7a792ab0c04a
Robert Zebec <zebec.robert@gmail.com>
2018-01-30 11:33:42 +0100

    Improved skeleton for Scanner

--- components/scanner/src/main/java/Scanner.java
+++ components/scanner/src/main/java/Scanner.java
@@ -2,18 +2,39 @@ public class Scanner {
     private static Scanner instance = new Scanner();
     public Port port;
 
-    public String getVersion() {
+    private Scanner() {
+        this.port = new Port();
+    }
+
+    public String innerGetVersion() {
         return "Scanner - Version 1.0";
     }
 
     public class Port implements IScanner {
 
         public boolean scan(Baggage baggage, String pattern) {
-            return false;
+            return innerScan(baggage, pattern);
         }
 
         public boolean scan(Passenger passenger, String pattern) {
-            return false;
+            return innerScan(passenger, pattern);
         }
+
+        /**
+         * Gets the version of the component.
+         *
+         * @return The version string.
+         */
+        public String getVersion() {
+            return innerGetVersion();
+        }
+    }
+
+    private boolean innerScan(Baggage baggage, String pattern) {
+        return false;
+    }
+
+    private boolean innerScan(Passenger passenger, String pattern) {
+        return false;
     }
 }
