44c25ce0cfc6178a80fb987776400ff800cd542e
tim <kuehnleintim@gmail.com>
2018-01-30 17:59:23 +0100

    factoryTests

--- main-application/src/main/Test/factory/TestGroundControlsCenterFactory.java
+++ main-application/src/main/Test/factory/TestGroundControlsCenterFactory.java
@@ -1,10 +1,6 @@
 package factory;
 
-import factory.GroundOperationsCenterFactory;
 import org.junit.Test;
-
-import java.lang.reflect.Method;
-
 import static org.junit.Assert.assertNotNull;
 
 public class TestGroundControlsCenterFactory {
@@ -14,17 +10,5 @@ public class TestGroundControlsCenterFactory {
     public void build() {
         componentPort = GroundOperationsCenterFactory.build();
         assertNotNull(componentPort);
-    }
-
-    @Test
-    public void receive() {
-        componentPort = GroundOperationsCenterFactory.build();
-        try {
-            //Method onMethod = componentPort.getClass().getDeclaredMethod("on");
-            //boolean isOn = (boolean)onMethod.invoke(componentPort);
-            //assertEquals(true,isOn);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
     }
 }
