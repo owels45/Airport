b4f37dfbdd65a0c821105e2fbfa96483fe59c12a
Marc <marc@h8m.eu>
2018-01-30 17:12:47 +0100

    merged

--- components/ground-operations-center/src/main/java/SupportClasses/CustomsReceipt.java
+++ components/ground-operations-center/src/main/java/SupportClasses/CustomsReceipt.java
@@ -1,14 +1,7 @@
-<<<<<<< HEAD:components/ground-operations-center/src/main/java/SupportClasses/CustomsReceipt.java
-<<<<<<< HEAD:components/customs/src/main/java/CustomsReceipt.java
+
+package SupportClasses;
 
 import java.util.ArrayList;
-=======
-package SupportClasses;
->>>>>>> origin/ground-operation-center-adi:components/ground-operations-center/src/main/java/SupportClasses/CustomsReceipt.java
-=======
-import java.util.ArrayList;
-import DummyFiles.*;
->>>>>>> FederalPolice:components/customs/src/main/java/CustomsReceipt.java
 
 public class CustomsReceipt {
     private int numberOfPassportVerified;
