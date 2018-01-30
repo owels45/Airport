85fe4262aafd13df8f895d5920b86a8519691914
tim <kuehnleintim@gmail.com>
2018-01-30 11:06:53 +0100

    GroundOperationsCenter commited

---
+++ components/ground-operations-center/src/main/java/GroundOperationsCenter.java
@@ -0,0 +1,17 @@
+public class GroundOperationsCenter {
+    ArrayList<CheckInDeskReceipt> checkInReceipts;
+    ArrayList<BulkyBaggageDeskReceipt> bulkyBaggageDeskReceipts;
+    ArrayList<BaggageSortingUnitReceipt> baggeSortingUnitReceipts;
+    ArrayList<SecurityCheckReceipt> securityCheckReceipts;
+    ArrayList<FederalPoliceReceipt> federalPoliceReceipts;
+    ArrayList<CustomReceipt> customsReceipts;
+    ArrayList<ServiceVehicleOilReceipt> serviceVehicleOilReceipts;
+    ArrayList<ServiceVehicleNitrogenOxygenReceipt> serviceVehicleNitrogenOxygenReceipts;
+    ArrayList<ServiceVehicleFreshWaterReceipt> serviceVehicleFreshWaterReceipts;
+    ArrayList<ServiceVehicleWasteWaterTankReceipt> serviceVehicleWasteWaterTankReceipts;
+    ArrayList<AirCargoPalletLifterReceipt> airCargoPalletLifterReceipts;
+    ArrayList<ContainerLifterReceipt> containerLifterReceipts;
+    ArrayList<FuelReceipt> fuelReceipts;
+    ArrayList<BoardingControlReceipt> boardingControlReceipts;
+    ArrayList<PushBackVehicleReceipt> pushBackVehicleReceipts;
+}
