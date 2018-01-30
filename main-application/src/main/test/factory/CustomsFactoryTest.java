20da416e583318a3c401f64a5107b445589a4808
Marc <marc@h8m.eu>
2018-01-30 20:14:57 +0100

    Fixed problems

--- main-application/src/main/test/factory/CustomsFactoryTest.java
+++ main-application/src/main/test/factory/CustomsFactoryTest.java
@@ -1,11 +1,9 @@
 package factory;
+
 import org.junit.jupiter.api.Test;
 
 import static junit.framework.TestCase.assertNotNull;
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
 
 public class CustomsFactoryTest {
 
