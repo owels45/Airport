d24280c4c5055102a9b178027153ebe823b0e25e
Robert Zebec <zebec.robert@gmail.com>
2018-01-30 11:37:52 +0100

    Added the getInstance() Method to scanner

--- components/scanner/src/main/java/Scanner.java
+++ components/scanner/src/main/java/Scanner.java
@@ -10,6 +10,10 @@ public class Scanner {
         return "Scanner - Version 1.0";
     }
 
+    public static Scanner getInstance() {
+        return instance;
+    }
+
     public class Port implements IScanner {
 
         public boolean scan(Baggage baggage, String pattern) {
@@ -35,6 +39,15 @@ public class Scanner {
     }
 
     private boolean innerScan(Passenger passenger, String pattern) {
+        if(passenger.content.contains(pattern)) {
+            return true;
+        }
+        for(Baggage baggage : passenger.baggageList) {
+            if(innerScan(baggage, pattern)) {
+
+            }
+        }
+
         return false;
     }
 }
