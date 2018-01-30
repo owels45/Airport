3298303dfcdd9cfe4ba8b72f79d6f4934a53eb77
tim <kuehnleintim@gmail.com>
2018-01-30 11:42:43 +0100

    GroundOperationsCenter commited

--- components/ground-operations-center/src/main/java/GroundOperationsCenter.java
+++ components/ground-operations-center/src/main/java/GroundOperationsCenter.java
@@ -1,10 +1,12 @@
-public class GroundOperationsCenter {
-    ArrayList<CheckInDeskReceipt> checkInReceipts;
-    ArrayList<BulkyBaggageDeskReceipt> bulkyBaggageDeskReceipts;
+import java.util.ArrayList;
+
+public class GroundOperationsCenter implements IGroundOperationsCenter {
+    ArrayList<CheckInDeskReceipt> checkInReceipts = new ArrayList<CheckInDeskReceipt>();
+    ArrayList<BulkyBaggageDeskReceipt> bulkyBaggageDeskReceipts = new ArrayList<BulkyBaggageDeskReceipt>();
     ArrayList<BaggageSortingUnitReceipt> baggeSortingUnitReceipts;
     ArrayList<SecurityCheckReceipt> securityCheckReceipts;
     ArrayList<FederalPoliceReceipt> federalPoliceReceipts;
-    ArrayList<CustomReceipt> customsReceipts;
+    ArrayList<CustomsReceipt> customsReceipts;
     ArrayList<ServiceVehicleOilReceipt> serviceVehicleOilReceipts;
     ArrayList<ServiceVehicleNitrogenOxygenReceipt> serviceVehicleNitrogenOxygenReceipts;
     ArrayList<ServiceVehicleFreshWaterReceipt> serviceVehicleFreshWaterReceipts;
@@ -14,4 +16,33 @@ public class GroundOperationsCenter {
     ArrayList<FuelReceipt> fuelReceipts;
     ArrayList<BoardingControlReceipt> boardingControlReceipts;
     ArrayList<PushBackVehicleReceipt> pushBackVehicleReceipts;
+
+
+    public void receive(CheckInDeskReceipt checkInReceipt) {
+        checkInReceipts.add(checkInReceipt);
+    }
+
+    public void receive(BulkyBaggageDeskReceipt bulkyBaggageDeskReceipt) {
+        bulkyBaggageDeskReceipts.add(bulkyBaggageDeskReceipt);
+    }
+
+    public void receive(BaggageSortingUnitReceipt baggageSortingUnitReceipt) {
+        baggeSortingUnitReceipts.add(baggageSortingUnitReceipt);
+    }
+
+    public void receive(SecurityCheckReceipt securityCheckReceipt) {
+        securityCheckReceipts.add(securityCheckReceipt);
+    }
+
+    public void receive(FederalPoliceReceipt federalPoliceReceipt) {
+        federalPoliceReceipts.add(federalPoliceReceipt);
+    }
+
+    public void receive(CustomsReceipt customsReceipt) {
+        customsReceipts.add(customsReceipt);
+    }
+
+    public void receive(ServiceVehicleOilReceipt serviceVehicleOilReceipt) {
+        serviceVehicleOilReceipts.add(serviceVehicleOilReceipt);
+    }
 }
