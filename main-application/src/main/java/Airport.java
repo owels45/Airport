52a9607cbd18a812c72495c1de4dd30bcb8c6038
Morten Terhart <morten.terhart@gmail.com>
2018-02-20 23:59:37 +0100

    Included factory build() method out of comment

--- main-application/src/main/java/Airport.java
+++ main-application/src/main/java/Airport.java
@@ -17,11 +17,8 @@ import event.service_vehicle_oil.ServiceVehicleChangeFireExtinguisher;
 import event.service_vehicle_oil.ServiceVehicleEngineOilTankIncreaseLevel;
 import event.service_vehicle_oil.ServiceVehicleRefillDeIcingSystem;
 import event.service_vehicle_waster_water.ServiceVehiclePumpOut;
-import factory.BoardingControlFactory;
-import factory.GroundOperationsCenterFactory;
-import factory.ServiceVehicleOilFactory;
+import factory.*;
 import logging.LogEngine;
-import sun.rmi.runtime.Log;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -52,13 +49,16 @@ public class Airport extends Subscriber {
 //        federalPolicePort = .build();
 //        customsPort = .build();
         serviceVehicleOilPort = ServiceVehicleOilFactory.build();
-//        serviceVehicleNitrogenOxygenPort = .build();
-//        serviceVehicleFreshWaterPort = .build();
-//        serviceVehicleWasteWaterTankPort = .build();
-//        airCargoPalletLifterPort = .build();
-//        skyTankingVehiclePort = SkyTankingVehicleFactory.build();
-        boardingControlPort = BoardingControlFactory.build();
-//        pushBackVehiclePort = .build();
+        serviceVehicleNitrogenOxygenPort = ServiceVehicleNitrogenOxygenFactory.build();
+        serviceVehicleFreshWaterPort = ServiceVehicleFreshWaterFactory.build();
+        serviceVehicleWasteWaterTankPort = ServiceVehicleWasteWaterTankFactory.build();
+//        airCargoPalletLifterPort = AirCargoPalletLifterFactory.build();// TODO: 20.02.2018 Factory missing!!!
+//        skyTankingVehiclePort = SkyTankingVehicleFactory.build(); // TODO: 20.02.2018 java.lang.ClassNotFoundException: main.SkyTankingVehicle
+        boardingControlPort = BoardingControlFactory.build(); // TODO: 20.02.2018 Abhängigkeit zu CheckInDesk??
+                                                              // TODO: 20.02.2018 Wird in BoardingControl benutzt, ist also Abhängigkeit
+                                                              //                  (in Maven als <dependency> hinzugefügt, um Dummy-Klassen
+                                                              //                   entfernen zu können, JAR bauen zu können und Komponente fertigzustellen)
+//        pushBackVehiclePort = PushBackVehicleFactory.build(); // TODO: 20.02.2018 what the heck???
     }
 
     // TODO: 01.02.2018  HIER DIE GANZEN SUBSCRIBE METHODEN VON JEDEM TEAM SELBST!!!
