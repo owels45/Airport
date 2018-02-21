8caf03b867cb75c876c25eedf388a9204a693af8
Morten Terhart <morten.terhart@gmail.com>
2018-02-21 21:35:30 +0100

    Added BoardingControlFactoryTest and dynamic building of jar path

--- main-application/src/main/java/configuration/Configuration.java
+++ main-application/src/main/java/configuration/Configuration.java
@@ -5,7 +5,7 @@ public enum Configuration {
 
     public String userDirectory = System.getProperty("user.dir");
     public String fileSeparator = System.getProperty("file.separator");
-    public String commonPathToJavaArchive = userDirectory + fileSeparator + "main-application" + fileSeparator + "components";
+    public String commonPathToJavaArchive = buildJarPath();
 
     public String lineSeparator = System.getProperty("line.separator");
     public String logFileDirectory = userDirectory + fileSeparator + "log" + fileSeparator;
@@ -13,4 +13,16 @@ public enum Configuration {
 
     public String dataDirectory = userDirectory + fileSeparator + "data" + fileSeparator;
     public String databaseFile = dataDirectory + "flightRecorderA380.db";
+
+    /**
+     * Builds the path to the jar archives depending on the current user directory.
+     *
+     * @return the correct path to the components folder.
+     */
+    private String buildJarPath() {
+        if (userDirectory.endsWith("main-application")) {
+            return userDirectory + fileSeparator + "components";
+        }
+        return userDirectory + fileSeparator + "main-application" + fileSeparator + "components";
+    }
 }
